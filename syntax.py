e73e86173ab2d316295f10051f5044b1e5e4bbbf
acantherie <116684304+acantherie@users.noreply.github.com>
2022-11-03 22:00:11 +0000

    started :)

---
+++ syntax.py
@@ -0,0 +1,97 @@
+# print("hello")
+# x = 224
+#print (x)
+# sjkdadklsdjl aaaaaaa
+# print(hello) this doesnt work lol
+#y = 123467899999999999
+#print(y)
+#k = does it work on letters .it does not work on letters
+#print(k)
+#print(x+y)
+#omg
+#pr = 10
+#print(pr)
+# two variables work 
+#qur = 46
+#print(qur)
+#x,y = 100, 567
+# animals = ["cow","sheep","bird","owl"]
+# x,y,z,l = animals 
+# print(x)
+# print(y)
+# print(z)
+# print(l)
+# print(x,y,z,l)
+
+# a = float(a) ??? imconfusedf
+# b = int(b) ??? maybe
+#a = 9.9
+#b = 14
+#print(a+b)
+
+#import random #this usually goes at the top
+#print(random.randrange(1,1000))
+#print(random.randrange(1,1000000))
+#print(random.randrange(0,1)) #doesnt give the number one? weird
+#print(random.randrange(1,int('100')))
+#print(random.randrange(-100,100))
+#print(random.randrange(1.456,3.999)) #*crowd boos*
+#e = 1.92
+#x = 4.23
+#e = (random.randrange(1,10))
+#print (e) #yipee
+#
+#greeting = 'yipee'
+#print(greeting[1])
+#print(greeting[0])
+#txt = "yipee"
+#print(txt[1:5])
+
+
+#print(15>10)
+#print(15 == 10)
+#print(15<10)
+
+#someTxt = "Green is the best color"
+#ok so this stuff is case sensitive
+#print("Green" in someTxt)
+#print("green" in someTxt)
+#print("Pink" in someTxt)
+
+#if statements yipee
+#a = 46
+#b = 98
+#if b>a:
+ # print("b is greater than a")
+#else:
+ # print("lol no")
+
+#print(bool("Pink"))
+#print(bool(78))
+#print(bool(0))
+#zero, none, false will output false
+#print(bool("yipee"))
+
+# def func():
+#  return False
+#if func():
+ # print("Yes")
+#else:
+ # print("No")
+
+#def func():
+#  return False
+#if func():
+ # print("Yes1")
+#else:
+#  print("Yes2")
+
+#x = 200
+#print(isinstance(x,str))
+#print(isinstance(x,int)) #yipee
+
+#print(5+5)
+#print(5 - 5)
+#print(5 * 5)
+#print(5/5)
+#print(5==5)
